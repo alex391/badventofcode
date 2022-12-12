25e99f8ba10e0bca821ac9418f3165198208d90c
Alex Leute <acl2809@rit.edu>
2022-12-11 23:15:30 -0500

    Part 2 done

--- 2022/day4/src/main.rs
+++ 2022/day4/src/main.rs
@@ -19,18 +19,35 @@ fn main() {
     }
 
     let mut count = 0;
-    for pair in elf_paris {
+    for pair in elf_paris.clone() {
         if check_bounds(pair) {
             count += 1;
         }
     }
-    println!("{count}");
+    println!("Part 1: {count}");
+
+    count = 0;
+    for pair in elf_paris {
+        if check_overlap(pair) {
+            count += 1;
+        }
+    }
+    println!("Part 2: {count}");
 }
 
-/// check the pairs overlap
-fn check_bounds(pair:[u32; 4]) -> bool{
+/// check if one pair entirely contains the other
+fn check_bounds(pair: [u32; 4]) -> bool{
     let upper_range = pair[2]..=pair[3];
     let lower_range = pair[0]..=pair[1];
     upper_range.contains(&pair[0]) && upper_range.contains(&pair[1]) ||
     lower_range.contains(&pair[2]) && lower_range.contains(&pair[3])
+}
+
+/// check if the pairs overlap at all
+fn check_overlap(pair: [u32; 4]) -> bool {
+    let upper_range = pair[2]..=pair[3];
+    let lower_range = pair[0]..=pair[1];
+    upper_range.contains(&pair[0]) || upper_range.contains(&pair[1]) ||
+    lower_range.contains(&pair[2]) || lower_range.contains(&pair[3])
+
 }
