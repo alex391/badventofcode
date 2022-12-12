43678661b80e61b437dea174bc981ccf3222d2a0
Alex Leute <acl2809@rit.edu>
2022-12-12 02:14:47 -0500

    Parsing this terible thing is complete!

---
+++ 2022/day5/src/main.rs
@@ -0,0 +1,48 @@
+use std::fs;
+
+fn main() {
+    let contents = fs::read_to_string("input.txt")
+        .expect("Should have been able to read the file");
+    let mut contents = contents.lines();
+
+    // Parse the input
+    const STACK_COUNT: usize = 9; // not worth trying to read this from the file
+
+    let mut stacks: [Vec<char>; STACK_COUNT] = Default::default();
+    
+    // skip the first character in a line, read the next, then skip 4
+    // repeat until the number of characters read is equal to STACK_COUNT
+    // ignore spaces
+    // repeat until contents doesn't start with a '['
+
+    loop {
+        let line = contents.next().unwrap();
+        if !line.starts_with('[') {
+            break;
+        }
+        let char_vec: Vec<(usize, char)> = line.chars()
+            .skip(1)
+            .enumerate()
+            .filter(|(index, _)| {
+                index % 4 == 0 
+            })
+            .collect();
+
+        for i in 0..STACK_COUNT {
+            let (_, c) = char_vec[i];
+            if c != ' ' {
+                stacks[i].insert(0, c);
+            }
+        }
+    }
+
+    // Then, parse the instructions
+
+    let mut instructions: Vec<Vec<u32>> = Vec::new();
+    for line in contents {
+        instructions.push(line.split(' ').filter_map(|string| {
+            string.parse().ok()
+        }).collect());
+    }
+
+}
