d15c441a86eed457e2203a9d05a7c7eeb7157d26
Alex Leute <acl2809@rit.edu>
2022-12-17 03:00:26 -0500

    part 2 done

--- 2022/day8/src/main.rs
+++ 2022/day8/src/main.rs
@@ -8,6 +8,16 @@ fn check_visibility(row: &[u32], index: usize) -> bool {
     row.iter().enumerate().filter(|&x| x.0 > index && *x.1 >= row[index]).count() == 0 // right side visibility
 }
 
+/// Calculate how many trees are visible, given a row to the right of it (or rotated so that it is)
+fn visible_trees(row: &[u32], height: u32) -> usize {
+    for tree in row.iter().enumerate() {
+        if height <= *tree.1 {
+            return tree.0 + 1;
+        }
+    }
+    row.len()
+}
+
 fn main() {
     let contents = fs::read_to_string("input.txt").expect("Should have been able to read the file");
     let contents = contents.lines();
@@ -41,4 +51,31 @@ fn main() {
         }
     }
     println!("Part 1: {count}");
+
+    let mut score = 0;
+    for y in 0..trees.len() {
+        for x in 0..trees[y].len() {
+            let mut left = trees[y].clone(); // Don't want to mutate the original vec
+            left = left[0..x].to_vec();
+            left.reverse();
+            let mut this_score = visible_trees(&left, trees[y][x]);// Left
+            this_score *= visible_trees(&trees[y][(x + 1)..], trees[y][x]); //Right
+            let mut column = Vec::new();
+            for row in &trees {
+                column.push(row[x])
+            }
+            let mut up = column.clone();
+            up = up[0..y].to_vec();
+            up.reverse();
+            this_score *= visible_trees(&up, trees[y][x]); // Up
+            this_score *= visible_trees(&column[(y + 1)..], trees[y][x]); // Down
+            if this_score > score {
+                score = this_score;
+            }
+
+        }
+    }
+    println!("Part 2: {score}");
+
+
 }
