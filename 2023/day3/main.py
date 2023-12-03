8ab3d9930467debda6979656b3c928a78506a9e0
Alex Leute <acleute@gmail.com>
2023-12-03 02:10:16 -0500

    Day 3 part 1

---
+++ 2023/day3/main.py
@@ -0,0 +1,86 @@
+import math
+class Part():
+    def __init__(self, part_number: int, coordinate: (int, int)):
+        self.part_number = part_number
+        self.coordinate = coordinate
+
+    def __hash__(self) -> int: # only unique if the coordinate is the same
+        return hash(self.coordinate)
+    
+    def __eq__(self, other) -> bool:
+        return self.coordinate == other.coordinate
+    
+    def __str__(self) -> str:
+        return f"({self.part_number}, ({self.coordinate[0]}, {self.coordinate[1]}))"
+    
+    def __repr__(self) -> str:
+        return self.__str__()
+
+def main():
+    engine = []
+    with open("input.txt") as file:
+        for line in file:
+            engine.append(list(line.strip()))
+
+    parts = set()
+    for y, row in enumerate(engine):
+        for x, column in enumerate(row):
+            if column != '.' and not column.isdecimal():
+                # Then it's a symbol
+                # check if there are numbers around it
+                for part in check_for_numbers(engine, x, y):
+                    parts.add(part)
+
+    visualization = engine
+    sum = 0
+    for part in parts:
+        for i in range(number_length(part.part_number)):
+            visualization[part.coordinate[1]][part.coordinate[0] + i] = "X"
+        sum += part.part_number
+
+    for y in visualization:
+        for x in y:
+            print(x, end="")
+    print()
+    print(sum)
+
+def check_for_numbers(engine: list, x: int, y: int) -> list:
+    """Return a list of the numbers around it"""
+    numbers_around_symbol = []
+    for dy in range(-1, 2):
+        for dx in range(-1, 2):
+            try:
+                if dx == 0 and dy == 0:
+                    continue
+                if engine[y + dy][x + dx].isdecimal():
+                    numbers_around_symbol.append(parse_number_in_engine(engine, x + dx, y + dy))
+            except IndexError:
+                continue
+    return numbers_around_symbol
+
+
+def parse_number_in_engine(engine: list, x: int, y: int) -> Part:
+    """
+        Read an number from the engine at the coordinates (backwards or forwards)
+        Also return the coordinate of the starting value
+    """
+    row = engine[y]
+    start = x
+    while row[start].isdecimal():
+        start -= 1
+    start += 1
+    
+    number_string = ""
+    i = start
+    while row[i].isdecimal():
+        number_string += row[i]
+        if i == len(row) - 1:
+            break
+        i += 1
+    return Part(int(number_string), (start, y))
+
+def number_length(n: int) -> int:
+    return int(math.log10(n) + 1)  # https://math.stackexchange.com/a/1771462 thanks https://math.stackexchange.com/users/73561/antoine
+
+if __name__ == "__main__":
+    main()
