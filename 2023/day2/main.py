7445e9a89242c4ce00499122f0c416e0a3ed83dd
Alex Leute <acleute@gmail.com>
2023-12-03 04:20:41 -0500

    Day 2 part 3 done

--- 2023/day2/main.py
+++ 2023/day2/main.py
@@ -1,7 +1,4 @@
 class Hand():
-    MAX_RED = 12
-    MAX_GREEN = 13
-    MAX_BLUE = 14
     def __init__(self, colors):
         self.red = colors["red"] if "red" in colors else 0
         self.green = colors["green"] if "green" in colors else 0
@@ -9,9 +6,9 @@ class Hand():
     
     def __repr__(self) -> str:
         return f"red: {self.red}, green: {self.green}, blue: {self.blue}"
-
-    def is_valid(self) -> bool:
-        return self.red <= self.MAX_RED and self.green <= self.MAX_GREEN and self.blue <= self.MAX_BLUE
+    
+    def power(self) -> int:
+        return self.red * self.green * self.blue
 
 def main():
     sum = 0
@@ -20,20 +17,23 @@ def main():
             id += 1
             hands_part = line.split(":")[1].strip()
             hand_strings = [hand.strip() for hand in hands_part.split(";")]
-            valid = True
+            game = []
             for string in hand_strings:
                 cubes = [cube.strip() for cube in string.split(",")]
                 counts_and_colors = {color: int(count) for count, color in [cube.split() for cube in cubes]}
                 hand = Hand(counts_and_colors)
-                if not hand.is_valid():
-                    valid = False
-                #print(id, hand, hand.is_valid())
-            if valid:
-                sum += id
-
-                
+                game.append(hand)
+            fewest_cubes = min_hand(game)
+            sum += fewest_cubes.power()
     print(sum)
 
+def min_hand(hands: list) -> Hand:
+    return Hand({
+        "red": max([hand.red for hand in hands]),
+        "green": max([hand.green for hand in hands]),
+        "blue": max([hand.blue for hand in hands])
+    })
+    
 
 
 if __name__ == "__main__":
