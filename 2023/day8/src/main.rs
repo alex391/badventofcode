2adf662ed48a4be2b6ac298389856131dcc82ffd
Alex Leute <acl2809@rit.edu>
2024-03-24 04:00:51 -0400

    A solution using LCM

--- 2023/day8/src/main.rs
+++ 2023/day8/src/main.rs
@@ -1,13 +1,16 @@
 use regex::{self, Regex};
-use std::{
-    fs, collections::HashMap,
-};
+use std::{collections::HashMap, fs};
 
 fn main() {
     let contents = fs::read_to_string("input.txt").expect("Should have been able to read the file");
     let mut contents = contents.lines();
 
-    let directions: Vec<char> = contents.next().expect("Directions should exist!").trim().chars().collect();
+    let directions: Vec<char> = contents
+        .next()
+        .expect("Directions should exist!")
+        .trim()
+        .chars()
+        .collect();
     contents.next();
 
     let re = Regex::new(r" = \(|, |\)").expect("Invalid regex!");
@@ -24,34 +27,52 @@ fn main() {
                 }
             })
             .collect();
-        maps.insert(node_split[0].to_string(), (node_split[1].to_string(), node_split[2].to_string()));
+        maps.insert(
+            node_split[0].to_string(),
+            (node_split[1].to_string(), node_split[2].to_string()),
+        );
     }
-    
 
-    let mut current_key: String = "AAA".to_string();
-    let mut current_value: &(String, String) = maps.get(&current_key).unwrap_or_else(|| panic!("{current_key} not in maps"));
-    let mut count = 0;
-    'outer: loop {
-        for c in &directions {
-            match c {
-                'L' => {
-                    current_key = current_value.0.to_string();
+    let starting_keys = maps.keys().filter(|x| x.ends_with('Z'));
+    let mut counts: Vec<u64> = Vec::new();
+    for key in starting_keys {
+        let mut key: String = key.clone();
+        let mut count = 0;
+        let mut current_value: &(String, String) = maps
+            .get(&key)
+            .unwrap_or_else(|| panic!("{key} not in maps"));
+        'outer: loop {
+            for c in &directions {
+                match c {
+                    'L' => {
+                        key = current_value.0.to_string();
+                    }
+                    'R' => {
+                        key = current_value.1.to_string();
+                    }
+                    _ => {
+                        panic!("Invalid direction");
+                    }
                 }
-                'R' => {
-                    current_key = current_value.1.to_string();
+                count += 1;
+                if key.ends_with('Z') {
+                    break 'outer;
                 }
-                _ => {
-                    panic!("Invalid direction");
-                }
+                current_value = maps
+                    .get(&key)
+                    .unwrap_or_else(|| panic!("{key} not in maps"));
             }
-            count += 1;
-            if current_key == "ZZZ" {
-                break 'outer;
-            }
-            current_value = maps.get(&current_key).unwrap_or_else(|| panic!("{current_key} not in maps"));
         }
+        counts.push(count);
     }
-
-    println!("{count}");
+    println!("{}", vec_lcm(counts))
 }
 
+fn vec_lcm(mut x: Vec<u64>) -> u64 {
+    match x.len() {
+        0 => panic!("Can't lcm with empty Vec"),
+        1 => x[0],
+        2 => num::integer::lcm(x[0], x[1]),
+        3.. => num::integer::lcm(x.pop().unwrap(), vec_lcm(x)),
+    }
+}
