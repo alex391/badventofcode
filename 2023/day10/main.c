c0d818313c77d217a0554f5251d66630e5d53ac9
Alex Leute <acl2809@rit.edu>
2024-04-21 02:21:14 -0400

    Now able to tell what pipe the start is

--- 2023/day10/main.c
+++ 2023/day10/main.c
@@ -2,74 +2,196 @@
 #include "lib/arena/arena.h"
 #include <stdint.h>
 #include <stdio.h>
+#include <stdbool.h>
 
 #define MAX_ROWS 150
 #define MAX_COLUMNS 150
 
-struct node {
-  struct node *forwards;
-  struct node *backwards;
+struct pipe_t {
+	char name;
+	int connections[2];
 };
 
-enum directions { NORTH, EAST, SOUTH, WEST, NONE };
-
-struct pipe_type {
-  enum directions first_outlet;
-  enum directions second_outlet;
+struct pipe_t_slice {
+	struct pipe_t *buffer;
+	size_t length;
 };
 
-struct pipe_type *character_to_type(char c, Arena* arena) {
-  struct pipe_type* type = arena_alloc(arena, sizeof(type));
-  switch (c) {
-  case '|':
-    
-  default:
-    type->first_outlet = NONE;
-    type->second_outlet = NONE;
-  }
-  return type;
+enum direction {
+	NORTH = 0,
+	EAST = 1,
+	SOUTH = 2,
+	WEST = 3,
+	NONE = 4,
+};
+
+struct pipe_t_slice *filter_pipes(const struct pipe_t *pipes, size_t size,
+				  int direction, Arena *a);
+bool pipe_t_in(struct pipe_t *needle, struct pipe_t_slice *haystack);
+const struct pipe_t pipe_types[] = {
+	{ '|', { NORTH, SOUTH } }, { '-', { EAST, WEST } },
+	{ 'L', { NORTH, EAST } },  { 'J', { NORTH, WEST } },
+	{ '7', { SOUTH, WEST } },  { 'F', { SOUTH, EAST } },
+	{ '.', { NONE, NONE } },
+};
+
+struct pipe_t *lookup_pipe(char pipe_name)
+{
+	// Just linear search
+	struct pipe_t *pipe = (struct pipe_t *)pipe_types;
+	while (pipe <
+	       (pipe_types + (sizeof(pipe_types) / sizeof(struct pipe_t)))) {
+		if (pipe->name == pipe_name) {
+			return pipe;
+		}
+		pipe++;
+	}
+	return pipe; // Just return the last one ('.')
 }
 
 // Find out which pipe the start is
-enum directions *connected_to_start(size_t start_x, size_t start_y,
-                                    char **grid) {
+struct pipe_t *start_type(size_t start_x, size_t start_y, char grid[][MAX_COLUMNS],
+			  size_t grid_height, size_t grid_width, Arena *b)
+{
+	Arena a = { 0 };
+	size_t connections_index = 0;
+	struct pipe_t *start = arena_alloc(b, sizeof(struct pipe_t));
+	start->name = 'S';
+	start->connections[0] = NONE;
+	start->connections[1] = NONE;
 
-  return NULL;
+	// Idea: check all four directions, and check if it can connect
+
+	// North:
+	// Is one of the south-facing pipes connected?
+	if (start_y >= 1) {
+		struct pipe_t_slice *south_pipes = filter_pipes(
+			pipe_types, sizeof(pipe_types) / sizeof(struct pipe_t), SOUTH, &a);
+		char up_pipe_name = grid[start_y - 1][start_x];
+		struct pipe_t *up_pipe = lookup_pipe(up_pipe_name);
+		// check if up_pipe is in south_pipes, and add it to the start
+		if (pipe_t_in(up_pipe, south_pipes)) {
+			start->connections[connections_index++] = NORTH;
+		}
+	}
+
+	// South
+	if (start_y + 1 < grid_height) {
+		struct pipe_t_slice *pipes = filter_pipes(
+			pipe_types, sizeof(pipe_types) / sizeof(struct pipe_t),
+			NORTH, &a);
+		char pipe_name = grid[start_y + 1][start_x];
+		struct pipe_t *pipe = lookup_pipe(pipe_name);
+		// check if up_pipe is in south_pipes, and add it to the start
+		if (pipe_t_in(pipe, pipes)) {
+			start->connections[connections_index++] = SOUTH;
+		}
+	}
+	
+	// East
+	if (start_x + 1 < grid_width) {
+		struct pipe_t_slice *pipes = filter_pipes(
+			pipe_types, sizeof(pipe_types) / sizeof(struct pipe_t),
+			WEST, &a);
+		char pipe_name = grid[start_y][start_x + 1];
+		struct pipe_t *pipe = lookup_pipe(pipe_name);
+		// check if up_pipe is in south_pipes, and add it to the start
+		if (pipe_t_in(pipe, pipes)) {
+			start->connections[connections_index++] = EAST;
+		}
+	}
+
+	// West
+	if (start_x >= 1) {
+		struct pipe_t_slice *pipes = filter_pipes(
+			pipe_types, sizeof(pipe_types) / sizeof(struct pipe_t),
+			EAST, &a);
+		char pipe_name = grid[start_y][start_x - 1];
+		struct pipe_t *pipe = lookup_pipe(pipe_name);
+		// check if up_pipe is in south_pipes, and add it to the start
+		if (pipe_t_in(pipe, pipes)) {
+			start->connections[connections_index++] = WEST;
+		}
+	}
+	arena_free(&a);
+	return start;
 }
 
-int main() {
-  FILE *fp;
-  fp = fopen("input.txt", "r");
-  if (fp == NULL) {
-    fprintf(stderr, "Couldn't open the file!");
-    return 1;
-  }
-  char grid[MAX_ROWS][MAX_COLUMNS];
-  int c;
-  size_t row = 0;
-  size_t column = 0;
-  size_t max_column = 0; // Because \nEOF resets column, store the max
-  size_t start_x, start_y;
-  while ((c = getc(fp)) != EOF) {
-    if (c == '\n') {
-      max_column = column; // Input is rectangular
-      column = 0;
-      row++;
-    } else {
-      column++;
-    }
-    if (c == 'S') {
-      start_x = column;
-      start_y = row;
-    }
-    if (row > MAX_ROWS || column > MAX_COLUMNS) {
-      fprintf(stderr, "Grid is out of range!");
-      return 1;
-    }
-    grid[row][column] = c;
-  }
+// Just a linear search
+bool int_in(int needle, const int *haystack, size_t size)
+{
+	for (size_t i = 0; i < size; i++) {
+		if (haystack[i] == needle) {
+			return true;
+		}
+	}
+	return false;
+}
 
-  struct node start;
-  start.forwards = NULL;
-  start.backwards = NULL;
+bool pipe_t_in(struct pipe_t *needle, struct pipe_t_slice *haystack)
+{
+	for (size_t i = 0; i < haystack->length; i++) {
+		if (haystack->buffer[i].name == needle->name) {
+			return true;
+		}
+	}
+	return false;
+}
+
+// Filter pipes by what direction they contain
+struct pipe_t_slice *filter_pipes(const struct pipe_t *pipes, size_t size,
+				  int direction, Arena *a)
+{
+	struct pipe_t_slice *filtered =
+		arena_alloc(a, sizeof(struct pipe_t_slice));
+	filtered->buffer = arena_alloc(a, sizeof(struct pipe_t_slice));
+	filtered->length = 0;
+	for (size_t i = 0; i < size; i++) {
+		if (int_in(direction, pipes[i].connections,
+			   2 /* the number of connections each pipe has */)) {
+			filtered->buffer[filtered->length++] = pipes[i];
+		}
+	}
+	return filtered;
+}
+
+int main()
+{
+	FILE *fp;
+	fp = fopen("input.txt", "r");
+	if (fp == NULL) {
+		fprintf(stderr, "Couldn't open the file!");
+		return 1;
+	}
+	char grid[MAX_ROWS][MAX_COLUMNS];
+	int c;
+	size_t row = 0;
+	size_t column = 0;
+	size_t max_column = 0; // Because \nEOF resets column, store the max
+	size_t start_x, start_y;
+	while ((c = getc(fp)) != EOF) {
+		if (c == '\n') {
+			if (column > max_column) {
+				max_column = column;
+			}
+			column = 0;
+			row++;
+		} else {
+			column++;
+		}
+		if (c == 'S') {
+			start_x = column;
+			start_y = row;
+		}
+		if (row > MAX_ROWS || column > MAX_COLUMNS) {
+			fprintf(stderr, "Grid is out of range!");
+			return 1;
+		}
+		grid[row][column] = c;
+	}
+	Arena scratch = { 0 };
+	struct pipe_t *start_pipe = start_type(start_x, start_y, grid, row, max_column, &scratch);
+	printf("%c\n", start_pipe->name);
+	printf("%d\n", start_pipe->connections[0]);
+	printf("%d\n", start_pipe->connections[1]);
 }
