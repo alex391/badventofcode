7a03707dc39cf412f7381c599029276b5dbcd812
Alex Leute <alex@alexleute.com>
2024-12-01 23:30:35 -0500

    Part 2 done using Counter

--- 2024/day1/day1.py
+++ 2024/day1/day1.py
@@ -1,4 +1,7 @@
-def main():
+from collections import Counter
+
+
+def part_one():
     left = []
     right = []
     with open("input.txt", "r") as f:
@@ -13,7 +16,27 @@ def main():
     for left, right in zip(left, right):
         distance_sum += abs(left - right)
 
-    print(distance_sum)
-    
+    print(f"Part 1: {distance_sum}")
+
+
+def part_two():
+    left = []
+    right = []
+    with open("input.txt", "r") as f:
+        for line in f:
+            slplit = line.split("   ")
+            left.append(int(slplit[0]))
+            right.append(int(slplit[1]))
+
+    similarity_score = 0
+    right_counter = Counter(right)
+
+    for item in left:
+        similarity_score += item * right_counter[item]
+
+    print(f"Part 2: {similarity_score}")
+
+
 if __name__ == "__main__":
-    main()
+    part_one()
+    part_two()
