80dcdcbb6d880fa264064a0a3559461c9b675f24
Alex Leute <acl2809@rit.edu>
2024-12-06 14:29:41 -0500

    visuization done, but not sure what the problem is?

--- 2024/day4/main.c
+++ 2024/day4/main.c
@@ -3,16 +3,19 @@
 #define WIDTH 10
 #define HEIGHT 10
 
+
 #define MASK_COUNT 8
 #define MASK_WIDTH 4
 #define MASK_HEIGHT 4
+
+char debug_grid[WIDTH + MASK_WIDTH][HEIGHT + MASK_HEIGHT] = { 0 };
 
 // characters X, M, A, S, or '.' for ignore
 // '\0' at the end
 //         mask count
 //         |           y
-//         |           |  x
-//         v           v  v
+//         |           |            x
+//         v           v            v
 char masks[MASK_COUNT][MASK_HEIGHT][MASK_WIDTH + 1] = { 
 	{
 		"XMAS", 
@@ -27,7 +30,7 @@ char masks[MASK_COUNT][MASK_HEIGHT][MASK_WIDTH + 1] = {
 		"...."
 	}, // 1
 	{
-		"X...", 
+		"X...",
 		"M...",
 		"A...",
 		"S..."
@@ -76,6 +79,9 @@ int check_masks(size_t search_x, size_t search_y, char word_search[HEIGHT][WIDTH
 				if (testing == '.') {
 					continue;
 				} 
+				if (search_x + x > WIDTH || search_y + y > HEIGHT) {
+					continue;
+				}
 				char word_search_letter = word_search[search_y + y][search_x + x];
 				if (word_search_letter == testing) {
 					matched++;
@@ -84,8 +90,17 @@ int check_masks(size_t search_x, size_t search_y, char word_search[HEIGHT][WIDTH
 
 		}
 		if (matched == 4) {
+			// Debug printing:
+			for (size_t y = 0; y < MASK_HEIGHT; y++) {
+				for (size_t x = 0; x < MASK_WIDTH; x++) {
+					char mask_value = masks[i][x][y];
+					if (mask_value == '.') {
+						continue;
+					}
+					debug_grid[y + search_y][x + search_x] = mask_value;
+				}
+			}
 			matches++;
-			printf("match at (%zu, %zu), mask %zu\n", search_x, search_y, i);
 		}
 	}
 
@@ -101,6 +116,14 @@ void part_one(char word_search[HEIGHT][WIDTH + 1])
 		}
 	}
 	printf("%d\n", count);
+
+	for (size_t y = 0; y < HEIGHT + MASK_HEIGHT; y++) {
+		for (size_t x = 0; x < WIDTH + MASK_WIDTH; x++) {
+			printf("%c", debug_grid[y][x] == 0? '.': debug_grid[y][x]);
+		}
+		printf("\n");
+	}
+
 }
 int main()
 {
