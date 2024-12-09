4a654da62be7a31050ca5449655af43563ac9661
Alex Leute <acl2809@rit.edu>
2024-12-08 22:18:59 -0500

    Figured out what the problem is: not doing
    diagonals correctly

--- 2024/day4/main.c
+++ 2024/day4/main.c
@@ -27,7 +27,11 @@ int check_x(size_t search_x, size_t search_y, char word_search[HEIGHT][WIDTH + 1
 	for(size_t j = 0; j < 8; j++) {
 		for(size_t i = 0, len = strlen(mas); i < len; i++) {
 			// Lookup in the table which offset we need to check (multiplied by i + 1):
-			int offset[2] = { direction_lookup[j][0] * (i + 1), direction_lookup[j][1]  * (i + 1) };
+			// This has a problem with diagonals (even j)
+			int offset[2] = { 
+				direction_lookup[j][0] * (i + 1), 
+				direction_lookup[j][1] * (i + 1)
+			};
 			// Check if that's the letter in MAS that we're expecting
 			// If it isn't, then skip adding one to matches
 			ptrdiff_t x = offset[0] + search_x;
@@ -39,6 +43,8 @@ int check_x(size_t search_x, size_t search_y, char word_search[HEIGHT][WIDTH + 1
 			if (y > HEIGHT || y < 0) {
 				goto skip_add;
 			}
+			printf("\tOffset: %d, %d\n", offset[0], offset[1]);
+			printf("\tChecking %td, %td, direction %zu\n", x, y, j);
 			
 			if(word_search[y][x] != mas[i]) {
 				goto skip_add;
@@ -49,6 +55,7 @@ int check_x(size_t search_x, size_t search_y, char word_search[HEIGHT][WIDTH + 1
 skip_add:
 		(void)0; // (do nothing in the skip_add case)
 	}
+	printf("%d matches at %zu, %zu\n", matches, search_x, search_y);
 	return matches;
 }
 
