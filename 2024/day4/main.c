1b8f91888022a7a13f65d42f9fafb313416ae602
Alex Leute <acl2809@rit.edu>
2024-12-08 21:52:10 -0500

    Well, here's another thing that doesn't work....

--- 2024/day4/main.c
+++ 2024/day4/main.c
@@ -1,109 +1,54 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+#include <stddef.h>
 
 #define WIDTH 10
 #define HEIGHT 10
 
-
-#define MASK_COUNT 8
-#define MASK_WIDTH 4
-#define MASK_HEIGHT 4
-
-char debug_grid[WIDTH + MASK_WIDTH][HEIGHT + MASK_HEIGHT] = { 0 };
-
-// characters X, M, A, S, or '.' for ignore
-// '\0' at the end
-//         mask count
-//         |           y
-//         |           |            x
-//         v           v            v
-char masks[MASK_COUNT][MASK_HEIGHT][MASK_WIDTH + 1] = { 
-	{
-		"XMAS", 
-		"....",
-		"....",
-		"...."
-	}, // 0
-	{
-		"SAMX", 
-		"....",
-		"....",
-		"...."
-	}, // 1
-	{
-		"X...",
-		"M...",
-		"A...",
-		"S..."
-	}, // 2
-	{
-		"S...", 
-		"A...",
-		"M...",
-		"X..."
-	}, // 3
-	{
-		"X...", 
-		".M..",
-		"..A.",
-		"...S"
-	}, // 4
-	{
-		"S...", 
-		".A..",
-		"..M.",
-		"...X"
-	}, // 5
-	{
-		"...X", 
-		"..M.",
-		".A..",
-		"S..."
-	}, // 6
-	{
-		"...S", 
-		"..A.",
-		".M..",
-		"X..."
-	}, // 7
-
-};
-
-int check_masks(size_t search_x, size_t search_y, char word_search[HEIGHT][WIDTH + 1])
+int check_x(size_t search_x, size_t search_y, char word_search[HEIGHT][WIDTH + 1])
 {
 	int matches = 0;
-	for (size_t i = 0; i < MASK_COUNT; i++) {
-		int matched = 0;
-		for (size_t y = 0; y < MASK_HEIGHT; y++) {
-			for (size_t x = 0; x < MASK_WIDTH; x++) {
-				char testing = masks[i][x][y];
-				if (testing == '.') {
-					continue;
-				} 
-				if (search_x + x > WIDTH || search_y + y > HEIGHT) {
-					continue;
-				}
-				char word_search_letter = word_search[search_y + y][search_x + x];
-				if (word_search_letter == testing) {
-					matched++;
-				}
+	const char *mas = "MAS";
+
+	const int direction_lookup[8][2] = {
+		// You could like, calculate this table, but eh, here it is:
+		{ 0, -1 },
+		{ 1, -1 },
+		{ 1, 0 },
+		{ 1, 1 },
+		{ 0, 1 },
+		{ -1, 1 },
+		{ -1, 0 },
+		{ -1, -1 },
+	};
+
+	// Check in all 8 directions for "MAS"
+	for(size_t j = 0; j < 8; j++) {
+		for(size_t i = 0, len = strlen(mas); i < len; i++) {
+			// Lookup in the table which offset we need to check (multiplied by i + 1):
+			int offset[2] = { direction_lookup[j][0] * (i + 1), direction_lookup[j][1]  * (i + 1) };
+			// Check if that's the letter in MAS that we're expecting
+			// If it isn't, then skip adding one to matches
+			ptrdiff_t x = offset[0] + search_x;
+			if (x > WIDTH || x < 0) {
+				goto skip_add;
 			}
 
-		}
-		if (matched == 4) {
-			// Debug printing:
-			for (size_t y = 0; y < MASK_HEIGHT; y++) {
-				for (size_t x = 0; x < MASK_WIDTH; x++) {
-					char mask_value = masks[i][x][y];
-					if (mask_value == '.') {
-						continue;
-					}
-					debug_grid[y + search_y][x + search_x] = mask_value;
-				}
+			ptrdiff_t y = offset[1] + search_y;
+			if (y > HEIGHT || y < 0) {
+				goto skip_add;
 			}
-			matches++;
+			
+			if(word_search[offset[1] + search_y][offset[1] + search_x] == mas[i]) {
+				goto skip_add;
+			}
+			
 		}
+		matches++;
+skip_add:
+		(void)0; // (do nothing in the skip_add case)
 	}
-
 	return matches;
 }
 
@@ -112,17 +57,14 @@ void part_one(char word_search[HEIGHT][WIDTH + 1])
 	int count = 0;
 	for (size_t y = 0; y < HEIGHT; y++) {
 		for (size_t x = 0; x < WIDTH; x++) {
-			count += check_masks(x, y, word_search);
+			// TODO: Only for Xs
+			if (word_search[y][x] == 'X') {
+				count += check_x(x, y, word_search);
+			}
 		}
 	}
 	printf("%d\n", count);
 
-	for (size_t y = 0; y < HEIGHT + MASK_HEIGHT; y++) {
-		for (size_t x = 0; x < WIDTH + MASK_WIDTH; x++) {
-			printf("%c", debug_grid[y][x] == 0? '.': debug_grid[y][x]);
-		}
-		printf("\n");
-	}
 
 }
 int main()
