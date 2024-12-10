40695a2feb7d06a8bd2ceab049e66cc9d3b4d577
Alex Leute <alex@alexleute.com>
2024-12-10 17:23:32 -0500

    Parse PageOrders

---
+++ 2024/day5/src/main.rs
@@ -0,0 +1,33 @@
+use std::{fs, num::ParseIntError, str::FromStr};
+
+struct PageOrder {
+    before: i32,
+    after: i32
+}
+
+impl FromStr for PageOrder {
+    type Err = String;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let mut split = s.split('|');
+        let before: Result<i32, ParseIntError> = split.next().ok_or("Can't parse before!")?.parse();
+        let before: i32 = match before {
+            Ok(i) => i,
+            Err(e) => return Err(e.to_string())
+        };
+
+        let after: Result<i32, ParseIntError> = split.next().ok_or("Can't parse after!")?.parse();
+        let after: i32 = match after {
+            Ok(i) => i,
+            Err(e) => return Err(e.to_string())
+        };
+
+        Ok(PageOrder {before, after})
+    }
+}
+
+fn main() {
+    let contents = fs::read_to_string("input.txt").expect("Should have been able to read the file");
+    let mut contents = contents.lines();
+
+}
