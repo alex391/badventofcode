a810950d75acc51beb48777da1b2355b07d6ca17
Alex Leute <alex@alexleute.com>
2024-12-11 17:19:41 -0500

    Part one, also part two will really easy

--- 2024/day5/src/main.rs
+++ 2024/day5/src/main.rs
@@ -1,8 +1,9 @@
-use std::{fmt::Display, fs, num::ParseIntError, str::FromStr};
+use std::{cmp::Ordering, fmt::Display, fs, num::ParseIntError, str::FromStr};
 
+#[derive(PartialEq, Eq)]
 struct PageOrder {
-    before: i32,
-    after: i32
+    a: i32,
+    b: i32,
 }
 
 impl FromStr for PageOrder {
@@ -13,27 +14,42 @@ impl FromStr for PageOrder {
         let before: Result<i32, ParseIntError> = split.next().ok_or("Can't parse before!")?.parse();
         let before: i32 = match before {
             Ok(i) => i,
-            Err(e) => return Err(e.to_string())
+            Err(e) => return Err(e.to_string()),
         };
 
         let after: Result<i32, ParseIntError> = split.next().ok_or("Can't parse after!")?.parse();
         let after: i32 = match after {
             Ok(i) => i,
-            Err(e) => return Err(e.to_string())
+            Err(e) => return Err(e.to_string()),
         };
 
-        Ok(PageOrder {before, after})
+        Ok(PageOrder { a: before, b: after })
     }
 }
 
 impl Display for PageOrder {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{}|{}", self.before, self.after)
+        write!(f, "{}|{}", self.a, self.b)
     }
 }
 
+fn page_order_cmp(a: i32, b: i32, page_orders: &[PageOrder]) -> std::cmp::Ordering {
+    let comparing = PageOrder {a, b};
+    let comparing_swapped = PageOrder {b, a};
+    for page_order in page_orders {
+        if comparing == *page_order {
+            return Ordering::Less;
+        }
+        if comparing_swapped == *page_order {
+            return Ordering::Greater;
+        }
+    }
+    Ordering::Equal
+}
+
 fn main() {
-    let contents = fs::read_to_string("input.txt").expect("Should have been able to read the file");
+    let contents =
+        fs::read_to_string("input.txt").expect("Should have been able to read input.txt!");
     let mut contents = contents.lines();
 
     let mut page_orders: Vec<PageOrder> = Vec::new();
@@ -48,21 +64,25 @@ fn main() {
             break; // Done with PageOrders
         }
         page_orders.push(line.parse().unwrap());
-
     }
 
     let mut updates: Vec<Vec<i32>> = Vec::new();
 
     for line in contents {
-        let update: Vec<i32> = line.split(',').map(|x| {x.parse().unwrap()}).collect();
+        let update: Vec<i32> = line.split(',').map(|x| x.parse().unwrap()).collect();
         updates.push(update);
     }
-    
-    for page_order in page_orders {
-        println!("{page_order}")
+
+    let mut sum = 0;
+
+    for mut update in updates {
+        let old_update = update.clone();
+        update.sort_unstable_by(|a, b| page_order_cmp(*a, *b, &page_orders));
+        if old_update == update {
+            let middle_index = update.len() / 2;
+            sum += update[middle_index];
+        }
     }
-    println!();
-    for update in updates {
-        println!("{:?}", update)
-    }
+    println!("{sum}");
+
 }
