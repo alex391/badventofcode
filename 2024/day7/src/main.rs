b2a3bd45bf99ee1f92dd4a33ee19ed8e1d4de798
Alex Leute <alex@alexleute.com>
2024-12-19 04:36:20 -0500

    Part 2 started

--- 2024/day7/src/main.rs
+++ 2024/day7/src/main.rs
@@ -1,3 +1,5 @@
+// Use cargo run --release for this!
+
 use std::{fs, str::FromStr};
 struct Calibration {
     expected: i64,
@@ -27,9 +29,27 @@ impl FromStr for Calibration {
     }
 }
 
-/// From the right (so is_bit_1(0b01, 0) returns true)
-fn is_bit_one(integer: u32, index: usize) -> bool {
-    (integer >> index) & 1 == 0
+/// From the right (so get_trit(4, 0) returns 1)  
+/// This is starting to remind me of a previous [SNAFU](https://adventofcode.com/2022/day/25)
+fn get_trit(mut integer: u32, index: usize) -> u8 {
+    // https://stackoverflow.com/a/3614138
+    // Also this could potentially be cached, if performance is bad
+    let mut trits: Vec<u8> = Vec::new();
+    while integer >= 3 {
+        let trit = integer % 3;
+        trits.push(trit as u8);
+        integer /= 3;
+    }
+    trits.push(integer as u8);
+    trits.reverse();
+    *trits.get(index).unwrap_or(&0)
+}
+
+fn concatenate(lhs: i64, rhs: i64) -> i64 {
+    // Could convert to string and back but this is slightly nicer
+    // https://stackoverflow.com/a/4483960
+    let rhs_length = rhs.ilog10() + 1;
+    (lhs * 10_i64.pow(rhs_length)) + rhs
 }
 
 fn main() {
@@ -39,17 +59,18 @@ fn main() {
     let mut sum = 0;
     for calibration in calibrations {
         // The idea is that we'll go through all of the combinations as if
-        // it's a binary number: + is 0, * is 1
+        // it's a ternary number: + is 0, * is 1, 2 is ||
         let operator_count = calibration.equation.len() - 1;
-        let max_operator_combo: u32 = 2u32.pow((operator_count).try_into().unwrap()) - 1; // operators = 2 -> 2^2 - 1 -> 0b100 - 1 -> 0b11
-
+        let max_operator_combo: u32 = 3u32.pow(operator_count as u32) - 1; // operators = 2 -> 3^2 - 1 -> 0t100 - 1 -> 0t22
         'outer: for operator_combo in 0..=max_operator_combo {
             let mut answer = calibration.equation[0];
             for operator_index in 0..operator_count {
                 let equation_index = operator_index + 1;
-                match is_bit_one(operator_combo, operator_index) {
-                    true => answer *= calibration.equation[equation_index],
-                    false => answer += calibration.equation[equation_index],
+                match get_trit(operator_combo, operator_index) {
+                    0 => answer += calibration.equation[equation_index],
+                    1 => answer *= calibration.equation[equation_index],
+                    2 => answer = concatenate(answer, calibration.equation[equation_index]),
+                    _ => unreachable!(),
                 }
             }
 
