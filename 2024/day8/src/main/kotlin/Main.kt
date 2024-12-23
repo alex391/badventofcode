a53d4e5cb443d9fb0bbfb905ef243b7da1524ab6
Alex Leute <alex@alexleute.com>
2024-12-23 04:42:47 -0500

    Part 2 done

--- 2024/day8/src/main/kotlin/Main.kt
+++ 2024/day8/src/main/kotlin/Main.kt
@@ -2,6 +2,8 @@ package org.example
 
 import java.io.FileNotFoundException
 
+typealias Point = Pair<Int, Int>
+
 class InputReader {
     /**
      * Read from the file
@@ -15,15 +17,46 @@ class InputReader {
 
 }
 
-fun pointDifference(a: Pair<Int, Int>, b: Pair<Int, Int>): Pair<Int, Int> {
-    return Pair(a.first - b.first, a.second - b.second)
+class Visualization(roof: List<String>) {
+    private val grid: MutableList<MutableList<Char>> = ArrayList()
+
+    init {
+        for (item in roof) {
+            grid.add(item.toMutableList())
+        }
+    }
+
+    fun addAntiNode(position: Point) {
+        if (grid[position.second][position.first] == '.') {
+            grid[position.second][position.first] = '#'
+        }
+    }
+
+    override fun toString(): String {
+        val stringBuilder = StringBuilder()
+        for (line in grid) {
+            for (c in line) {
+                stringBuilder.append(c)
+            }
+            stringBuilder.append('\n')
+        }
+        return stringBuilder.toString()
+    }
 }
 
-fun pointSum(a: Pair<Int, Int>, b: Pair<Int, Int>): Pair<Int, Int> {
-    return Pair(a.first + b.first, a.second + b.second)
+fun pointDifference(a: Point, b: Point): Point {
+    return Point(a.first - b.first, a.second - b.second)
 }
 
-fun isPointInBounds(point: Pair<Int, Int>, width: Int, height: Int): Boolean {
+fun pointSum(a: Point, b: Point): Point {
+    return Point(a.first + b.first, a.second + b.second)
+}
+
+fun pointScale(point: Point, scale: Int): Point {
+    return Point(point.first * scale, point.second * scale)
+}
+
+fun isPointInBounds(point: Point, width: Int, height: Int): Boolean {
     return when {
         point.first < 0 -> false
         point.second < 0 -> false
@@ -36,7 +69,9 @@ fun isPointInBounds(point: Pair<Int, Int>, width: Int, height: Int): Boolean {
 fun main() {
     val roof: List<String> = InputReader().readInput()
 
-    val nodeFrequencies: MutableMap<Char, MutableSet<Pair<Int, Int>>> = HashMap()
+    val visualization = Visualization(roof)
+
+    val nodeFrequencies: MutableMap<Char, MutableSet<Point>> = HashMap()
     for ((y, line) in roof.withIndex()) {
         for ((x, c) in line.withIndex()) {
             if (c == '.') {
@@ -45,11 +80,11 @@ fun main() {
             if (nodeFrequencies[c] == null) {
                 nodeFrequencies[c] = HashSet()
             }
-            nodeFrequencies[c]!!.add(Pair(x, y))
+            nodeFrequencies[c]!!.add(Point(x, y))
         }
     }
 
-    val antiNodes: MutableSet<Pair<Int, Int>> = HashSet()
+    val antiNodes: MutableSet<Point> = HashSet()
     for (frequency in nodeFrequencies) {
         // https://stackoverflow.com/q/62593231
         for (a in frequency.component2()) {
@@ -58,17 +93,35 @@ fun main() {
                     continue
                 }
 
-                val antiNodeOne = pointSum(pointDifference(a, b), a)
-                if (isPointInBounds(antiNodeOne, roof[0].length, roof.size)) {
-                    antiNodes.add(antiNodeOne)
+                val aDifference = pointDifference(a, b)
+                var i = 0
+                while (true) {
+                    val antiNode = pointSum(pointScale(aDifference, i), a)
+                    if (isPointInBounds(antiNode, roof[0].length, roof.size)) {
+                        antiNodes.add(antiNode)
+                        visualization.addAntiNode(antiNode)
+                    } else {
+                        break
+                    }
+                    i++
+
                 }
-                val antiNodeTwo = pointSum(pointDifference(b, a), b)
-                if (isPointInBounds(antiNodeTwo, roof[0].length, roof.size)) {
-                    antiNodes.add(antiNodeTwo)
+                i = 0
+                val bDifference = pointDifference(b, a)
+                while (true) {
+                    val antiNode = pointSum(pointScale(bDifference, i), b)
+                    if (isPointInBounds(antiNode, roof[0].length, roof.size)) {
+                        antiNodes.add(antiNode)
+                        visualization.addAntiNode(antiNode)
+                    } else {
+                        break
+                    }
+                    i++
                 }
             }
         }
     }
+    println(visualization)
     println(antiNodes.size)
 
 }
