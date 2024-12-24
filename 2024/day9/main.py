4dfe5c26b1eca2cdd89ba8426fe22f36f864ed01
Alex Leute <alex@alexleute.com>
2024-12-24 03:05:46 -0500

    Ran black
    Also wowie pypy3 is a lot faster than python3

--- 2024/day9/main.py
+++ 2024/day9/main.py
@@ -1,10 +1,11 @@
-# Use pypy3
+# Use pypy3 for 31x speedup
 from typing import NamedTuple
 
 
 class File(NamedTuple):
     index: int
     length: int
+
 
 def checksum(disk: list[int]) -> int:
     sum = 0
@@ -19,6 +20,7 @@ def print_no_spaces(disk: list[int]):
         print(block if block >= 0 else ".", end="")
     print()
 
+
 def has_space_for(file: File, index: int, disk: list[int]) -> bool:
     for offset in range(file.length):
         if offset + index > file.index:
@@ -30,7 +32,7 @@ def has_space_for(file: File, index: int, disk: list[int]) -> bool:
 
 def main():
     disk: list[int] = []  # ID or -1 for blank
-    file_table: list[File] = [] # table from ID to File
+    file_table: list[File] = []  # table from ID to File
     id = 0
     is_free = True
     with open("input.txt", "r") as f:
@@ -62,19 +64,18 @@ def main():
                 break
             if has_space:
                 for i in range(file.length):
-                    disk[left_index + i], disk[file.index + i] = disk[file.index + i], -1
+                    disk[left_index + i], disk[file.index + i] = (
+                        disk[file.index + i],
+                        -1,
+                    )
                 # print_no_spaces(disk)
                 left_index = 0
-                break # We found space for this file!
+                break  # We found space for this file!
             else:
                 left_index += 1
                 if left_index >= len(disk):
                     left_index = 0
                     break
-
-
-
-
 
     # print_no_spaces(disk)
 
