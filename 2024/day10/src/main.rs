c14936106b29e78159732c2cb279ff51af940604
Alex Leute <alex@alexleute.com>
2024-12-28 03:43:12 -0500

    The rare "part 2 is easier than part 1"
    This reverts commit 2924685d58986f7b6074bab0a38118f0b349b670.

--- 2024/day10/src/main.rs
+++ 2024/day10/src/main.rs
@@ -1,24 +1,17 @@
-use std::{collections::{HashSet, VecDeque}, fs, mem};
+use std::{fs, mem};
 
 const NEIGHBOR_OFFSETS: [(isize, isize); 4] = [(0, -1), (1, 0), (0, 1), (-1, 0)];
 
-#[derive(Default, Eq, Hash, PartialEq)]
+#[derive(Default)]
 struct Node {
-    x: isize, // Storing x and y just for Hash
-    y: isize,
     height: u32,
-    marked: bool,
     children: Vec<Node>,
 }
 
 impl Node {
-    
-    fn new(height: u32, x: isize, y: isize) -> Self {
+    fn new(height: u32) -> Self {
         Node {
             height,
-            x,
-            y,
-            marked: false,
             children: Vec::new(),
         }
     }
@@ -42,7 +35,7 @@ fn add_neighbors(grid: &[Vec<u32>], x: usize, y: usize, node: &mut Node) {
         if neighbor_x < 0 || neighbor_x as usize >= grid[neighbor_y as usize].len() {
             continue;
         }
-        let mut child = Node::new(grid[neighbor_y as usize][neighbor_x as usize], neighbor_x, neighbor_y);
+        let mut child = Node::new(grid[neighbor_y as usize][neighbor_x as usize]);
         let added = node.add_child(&mut child);
         drop(child); // Dropping here because this node just has default values so we probably don't want to use it later
         if !added {
@@ -58,30 +51,16 @@ fn add_neighbors(grid: &[Vec<u32>], x: usize, y: usize, node: &mut Node) {
     }
 }
 
-fn count_reachable_nines(start: Node) -> usize {
-    // https://youtu.be/xlVX7dXLS64?si=DhWzmFe13AqBIbOI
-    let mut queue: VecDeque<Node> = VecDeque::new();
-    let mut counted: HashSet<Node> = HashSet::new();
-    queue.push_back(start);
-
-    while !queue.is_empty() {
-        let mut v = queue.pop_front().unwrap();
-        if !v.marked {
-            if v.height == 9 {
-                counted.insert(v);
-                continue;
-            }
-            v.marked = true;
-            for w in v.children {
-                if !w.marked {
-                    queue.push_back(w);
-                }
-            }
-        }
+fn count_reachable_nines(start: Node) -> i32 {
+    // TODO: this over-counts, bfs?
+    if start.height == 9 {
+        return 1;
     }
-
-    counted.len()
-
+    let mut sum = 0;
+    for child in start.children {
+        sum += count_reachable_nines(child);
+    }
+    sum
 }
 
 fn main() {
@@ -95,7 +74,7 @@ fn main() {
     for (y, row) in grid.iter().enumerate() {
         for (x, height) in row.iter().enumerate() {
             if *height == 0 {
-                starts.push(Node::new(*height, x as isize, y as isize));
+                starts.push(Node::new(*height));
                 let start_index = starts.len() - 1;
                 add_neighbors(grid.as_slice(), x, y, &mut starts[start_index]);
             }
