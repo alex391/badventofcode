1c7181484b01b8bf9368fe231b1465241030ffd3
Alex Leute <alex@alexleute.com>
2024-12-31 15:25:01 -0500

    Work on part 2

--- 2024/day12/src/main.rs
+++ 2024/day12/src/main.rs
@@ -3,7 +3,7 @@ use std::{
     fs,
 };
 
-const NEIGHBOR_OFFSETS: [(isize, isize); 4] = [(0, -1), (1, 0), (0, 1), (-1, 0)];
+const NEIGHBOR_OFFSETS: [IPoint; 4] = [IPoint(0, -1), IPoint(1, 0), IPoint(0, 1), IPoint(-1, 0)];
 
 #[derive(Debug)]
 struct Price {
@@ -18,13 +18,63 @@ impl Price {
 }
 
 #[derive(Hash, Eq, PartialEq, Clone, Copy)]
-struct Point(usize, usize);
+struct UPoint(usize, usize);
 
-//
-fn flood_fill(masked_garden: &mut [Vec<bool>], start: Point) -> Price {
+impl UPoint {
+    fn to_ipoint(self) -> Option<IPoint> {
+        let zero = isize::try_from(self.0);
+        let one = isize::try_from(self.1);
+
+        if let Ok(zero) = zero {
+            if let Ok(one) = one {
+                return Some(IPoint(zero, one));
+            }
+        }
+        // This None case is ridiculous: .0 and .1 are <= 140, and, regardless, array indexes are required to be <= isize::MAX.
+        None
+    }
+}
+
+#[derive(Hash, Eq, PartialEq, Clone, Copy)]
+struct IPoint(isize, isize);
+
+impl IPoint {
+    fn to_upoint(self) -> Option<UPoint> {
+        let zero = usize::try_from(self.0);
+        let one = usize::try_from(self.1);
+
+        if let Ok(zero) = zero {
+            if let Ok(one) = one {
+                return Some(UPoint(zero, one));
+            }
+        }
+        None // Unlike UPoint's to_ipoint, this function is likely to return None
+    }
+
+    fn add(&self, rhs: &IPoint) -> IPoint {
+        IPoint(self.0 + rhs.0, self.1 + rhs.1)
+    }
+
+    /// Are both .0 and .1 < max and >= 0?
+    fn in_bounds(&self, max: usize) -> bool {
+        if max > isize::MAX as usize {
+            return self.0 >= 0 && self.1 > 0;
+        }
+        let max = max as isize;
+        self.0 < max && self.0 >= 0 && self.1 < max && self.1 >= 0
+    }
+}
+
+
+/// Return the number of corners that are here (0, 1, or 2)
+fn detect_corner(masked_garden: &[Vec<bool>], point_outside: IPoint) -> i32 {
+    
+}
+
+fn flood_fill(masked_garden: &mut [Vec<bool>], start: UPoint) -> Price {
     // https://youtu.be/xlVX7dXLS64
-    let mut queue: VecDeque<Point> = VecDeque::new();
-    let mut visited: HashSet<Point> = HashSet::new();
+    let mut queue: VecDeque<UPoint> = VecDeque::new();
+    let mut visited: HashSet<UPoint> = HashSet::new();
     let mut price: Price = Price {
         area: 0,
         perimeter: 0,
@@ -36,39 +86,15 @@ fn flood_fill(masked_garden: &mut [Vec<bool>], start: Point) -> Price {
             price.area += 1;
             visited.insert(v);
             for offset in NEIGHBOR_OFFSETS {
-                let neighbor_x: usize = match usize::try_from(v.0 as isize + offset.0) {
-                    Ok(x) => x,
-                    Err(_) => {
-                        price.perimeter += 1;
-                        continue;
-                    }
-                };
-                let neighbor_y: usize = match usize::try_from(v.1 as isize + offset.1) {
-                    Ok(y) => y,
-                    Err(_) => {
-                        price.perimeter += 1;
-                        continue;
-                    }
-                };
-                let neighbor_row = match masked_garden.get(neighbor_y) {
-                    Some(row) => row,
-                    None => {
-                        price.perimeter += 1;
-                        continue;
-                    }
-                };
-                let neighbor: bool = match neighbor_row.get(neighbor_x) {
-                    Some(n) => *n,
-                    None => {
-                        price.perimeter += 1;
-                        continue;
-                    }
-                };
-                if neighbor {
-                    queue.push_back(Point(neighbor_x, neighbor_y));
-                } else {
-                    price.perimeter += 1;
-                }
+                let v = v.to_ipoint().unwrap();
+                let neighbor = v.add(&offset);
+
+                todo!("detect_corner here");
+
+                queue.push_back(match neighbor.to_upoint() {
+                    Some(x) => x,
+                    None => continue,
+                });
             }
         }
     }
@@ -100,7 +126,7 @@ fn main() {
         for y in 0..masked_garden.len() {
             for x in 0..masked_garden[y].len() {
                 if masked_garden[y][x] {
-                    let price = flood_fill(&mut masked_garden, Point(x, y));
+                    let price = flood_fill(&mut masked_garden, UPoint(x, y));
                     price_total += price.get_price();
                     // println!("{:?}", price);
                 }
