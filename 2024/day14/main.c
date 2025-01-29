ebbbf1bcb6aa4a87ed7a58461da8e85e5f9071b6
Alex Leute <acl2809@rit.edu>
2025-01-29 17:19:13 -0500

    Done reading from the file

--- 2024/day14/main.c
+++ 2024/day14/main.c
@@ -2,15 +2,11 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
-
-struct point {
-	int x;
-	int y;
-};
-
 struct robot {
-	struct point p;
-	struct point v;
+	int px;
+	int py;
+	int vx;
+	int vy;
 };
 
 size_t count_lines(FILE *f)
@@ -33,15 +29,31 @@ int main()
 		exit(1);
 	}
 	size_t lines = count_lines(f);
-	rewind(f);
+	if (fseek(f, 0, SEEK_SET) == -1) {
+		fprintf(stderr, "File error: Could not seek to the start\n");
+		exit(2);
+	}
 	struct robot *robots = malloc(lines * sizeof(struct robot));
 	struct robot *this_robot = robots;
 	while(true) {
-		int s = fscanf(f, "p=%d,%d v=%d,%d", &this_robot->p.x,
-				&this_robot->p.y, &this_robot->v.x, &this_robot->v.y);
-		if (s == EOF) {
-			break;
+		int px = 0;
+		int py = 0;
+		int vx = 0;
+		int vy = 0;
+		char buff[20] = { 0 };
+		if (fgets(buff, sizeof(buff), f) == NULL) {
+			if (feof(f)) {
+				break;
+			}
+			fprintf(stderr, "File error: fgets read error");
+			exit(3);
 		}
+		sscanf(buff, "p=%d,%d v=%d,%d", &px, &py, &vx, &vy);
+		this_robot->px = px;
+		this_robot->py = py;
+		this_robot->vx = vx;
+		this_robot->vy = vy;
+
 		this_robot++;
 	}
 	fclose(f);
